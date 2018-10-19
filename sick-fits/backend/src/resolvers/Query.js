d986d42e48f4895061ebd98b8a58ff0c4f439b60
cjaredm <cjared.mortenson@gmail.com>
2018-10-19 13:59:08 -0600

    Setting up Prisma and GraphQL

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,16 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+/*
+forwardTo just sends the method to prisma.graphql and uses the method with the same name there.
+If you want to write your own, as we will later, we'll do it as the commented out example below.
+*/
+
+const Query = {
+  items: forwardTo("db")
+  // async items(partent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
